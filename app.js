e51d9aab36fe8f9d32a71d08a57b6a41eb57ceca
Adrian S <adriansaporta@gmail.com>
2020-02-17 22:28:31 -0500

    an mvp

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-const Manager = require("./lib/manager")
+
+const Manager = require('./lib/manager.js')
 const Engineer = require("./lib/engineer.js")
-const Intern = require("./lib/intern")
+const Intern = require("./lib/intern.js")
 const inquirer = require("inquirer")
-const path = require("path")
+// const path = require("path")
 const fs = require("fs")
 
-//************************************************************************* */
-// import Engineer from './lib/engineer.js'; // i don't think it's needed
 
-const engineer01 = new Engineer("john");
-console.log("module worked if name shows up: " + engineer01.engineerName)
-//************************************************************************* */
 
 
 
@@ -43,7 +39,7 @@ function appMenu() {
             },
             {
                 type: "input",
-                name: "memberType",
+                name: "nextMemberType",
                 message: "What type of member would you like to add?"
             }
         ])
@@ -52,18 +48,21 @@ function appMenu() {
                 console.log(managerObject);
                 //testing constructor here below..
                 manager01 = new Manager(managerObject.managerName, managerObject.managerId, managerObject.managerEmail, managerObject.managerOffice)
-                console.log("manager01 :" + manager01.managerEmail) //to see an example
-
-                if (answers.memberType === "engineer") {
+                console.log("manager01 :" + `email: ${manager01.managerEmail}, office number: ${manager01.managerOffice}`) //to see an example
+                writeFileManager();
+                if (answers.nextMemberType === "engineer") {
                     console.log("chose engineer")
                     createEngineer();
+
                 }
-                else if (answers.memberType === "intern") {
+                else if (answers.nextMemberType === "intern") {
                     console.log("chose intern")
                     createIntern();
                 }
 
                 else {
+                    writeEndHTML();
+
                     console.log("thanks for using this program!")
                 }
 
@@ -71,8 +70,88 @@ function appMenu() {
     }
 }
 
+
 appMenu();
 
+//Writes beginning of HTML and Manager profile with 
+function writeFileManager() {
+    fs.writeFile("main.html", `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+      <title>Document</title>
+    </head>
+    <body>
+          <div class="container">
+              <h1 class="display-4">Hi! My name is ${manager01.managerName}</h1>
+              <p class="lead">Id:${manager01.managerId}.</p>
+              <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+              <ul class="list-group">
+                  <li class="list-group-item">My email is ${manager01.managerEmail}</li>
+                  <li class="list-group-item">Office: ${manager01.managerOffice}</li>
+              </ul>
+          </div>` , function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        // console.log("The file was saved!");
+    });
+}
+
+//Writes Engineer profile html code  
+function writeFileEngineer() {
+    fs.appendFile("main.html", `
+    
+          <div class="container">
+              <h1 class="display-4">Hi! My name is ${engineer.engineerName}</h1>
+              <p class="lead">Id:${engineer.engineerId}.</p>
+              <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+              <ul class="list-group">
+                  <li class="list-group-item">My email is ${engineer.engineerEmail}</li>
+                  <li class="list-group-item">Office: ${engineer.engineerOffice}</li>
+              </ul>
+          </div>` , function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        // console.log("The file was saved!");
+    });
+}
+
+function writeFileIntern() {
+    fs.appendFile("main.html", `
+    
+    <div class="container">
+        <h1 class="display-4">Hi! My name is ${intern.internName}</h1>
+        <p class="lead">Id:${intern.internId}.</p>
+        <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+        <ul class="list-group">
+            <li class="list-group-item">My email is ${intern.internEmail}</li>
+            <li class="list-group-item">Office: ${intern.internOffice}</li>
+        </ul>
+    </div>` , function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        // console.log("The file was saved!");
+    });
+}
+
+function writeEndHTML() {
+    fs.appendFile("main.html", `
+    
+    </body>
+    </html>` , function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        // console.log("The file was saved!");
+    });
+
+}
 
 function createEngineer() {
 
@@ -104,6 +183,11 @@ function createEngineer() {
         }
     ])
         .then(function (answers) {
+            const engineerObject = answers;
+            console.log(engineerObject);
+            //testing constructor here below..
+            engineer = new Engineer(engineerObject.engineerName, engineerObject.engineerId, engineerObject.engineerEmail, engineerObject.engineerOffice)
+            writeFileEngineer();
             if (answers.memberType === "engineer") {
                 console.log("chose engineer")
                 createEngineer();
@@ -113,6 +197,8 @@ function createEngineer() {
                 createIntern();
             }
             else {
+                writeEndHTML();
+
                 console.log("thanks for using this program!")
             }
 
@@ -149,6 +235,11 @@ function createIntern() {
         }
     ])
         .then(function (answers) {
+            const internObject = answers;
+            console.log(internObject);
+            //testing constructor here below..
+            intern = new Intern(internObject.internName, internObject.internId, internObject.internEmail, internObject.internOffice)
+            writeFileIntern();
             if (answers.memberType === "engineer") {
                 console.log("chose engineer")
                 createEngineer();
@@ -158,8 +249,15 @@ function createIntern() {
                 createIntern();
             }
             else {
+                writeEndHTML();
+
                 console.log("thanks for using this program!")
             }
 
         });
 }
+
+// collect data from user with inq
+
+// use fs to write a html document
+
